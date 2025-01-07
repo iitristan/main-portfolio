150f6f93a5994b777a6ec4407877327692e327f8
tristan <88244585+iitristan@users.noreply.github.com>
2025-01-07 19:28:00 +0800

    small optimization and stat changes

--- backend/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-import connectDB from './config/db';
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
