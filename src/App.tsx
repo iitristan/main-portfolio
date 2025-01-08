85adf89219f6f2c5d09911e5a5ae45113ece9ec8
tristan <88244585+iitristan@users.noreply.github.com>
2025-01-08 23:27:16 +0800

    changed resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
-import { Analytics } from "@vercel/analytics/react"
+
 import Layout from "./components/Header";
 
 export default function App() {
